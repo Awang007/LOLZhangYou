9eab4b0cf6da53d06d0ac81e57b12fef64babab7
125154454 <a125154454@wenxing-shi.local>
2016-10-30 21:35:37 +0800

    创建项目

---
+++ LOLZhangyouBao/LOLZhangyouBao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOLZhangyouBao
+//
+//  Created by 125154454 on 16/10/30.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
